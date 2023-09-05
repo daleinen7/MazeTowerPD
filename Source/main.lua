f3d8c728103cd75e4e36bad576bb5dd062016074
Doug Leinen <doug@geletkaplus.com>
2023-09-04 19:55:28 -0500

    try to do tilemapping

--- Source/main.lua
+++ Source/main.lua
@@ -13,37 +13,42 @@ local playerSprite = nil
 
 gfx.setColor(gfx.kColorBlack)
 
+
+-- 
+
+
+
+
 function myGameSetUp()
 
-    -- Set up the player sprite.
-    -- The :setCenter() call specifies that the sprite will be anchored at its center.
-    -- The :moveTo() call moves our sprite to the center of the display.
+  -- Set up the player sprite.
+  -- The :setCenter() call specifies that the sprite will be anchored at its center.
+  -- The :moveTo() call moves our sprite to the center of the display.
 
-    local playerImage = gfx.image.new('Images/playerImage')
-    assert( playerImage ) -- make sure the image was where we thought
+  local playerImage = gfx.image.new('Images/playerImage')
+  assert( playerImage ) -- make sure the image was where we thought
 
-    playerSprite = gfx.sprite.new( playerImage )
-    playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
-    playerSprite:add() -- This is critical!
+  playerSprite = gfx.sprite.new( playerImage )
+  playerSprite:moveTo( 200, 120 ) -- this is where the center of the sprite is placed; (200,120) is the center of the Playdate screen
+  playerSprite:add() -- This is critical!
 
-    -- We want an environment displayed behind our sprite.
-    -- There are generally two ways to do this:
-    -- 1) Use setBackgroundDrawingCallback() to draw a background image. (This is what we're doing below.)
-    -- 2) Use a tilemap, assign it to a sprite with sprite:setTilemap(tilemap),
-    --       and call :setZIndex() with some low number so the background stays behind
-    --       your other sprites.
+  -- We want an environment displayed behind our sprite.
+  -- There are generally two ways to do this:
+  -- 1) Use setBackgroundDrawingCallback() to draw a background image. (This is what we're doing below.)
+  -- 2) Use a tilemap, assign it to a sprite with sprite:setTilemap(tilemap),
+  --       and call :setZIndex() with some low number so the background stays behind
+  --       your other sprites.
 
-    local backgroundImage = gfx.image.new( 'Images/background' )
-    assert( backgroundImage )
+  local backgroundImage = gfx.image.new( 'Images/background' )
+  assert( backgroundImage )
 
-    gfx.sprite.setBackgroundDrawingCallback(
-        function( x, y, width, height )
-            -- x,y,width,height is the updated area in sprite-local coordinates
-            -- The clip rect is already set to this area, so we don't need to set it ourselves
-            backgroundImage:draw( 0, 0 )
-        end
-    )
-
+  gfx.sprite.setBackgroundDrawingCallback(
+    function( x, y, width, height )
+      -- x,y,width,height is the updated area in sprite-local coordinates
+      -- The clip rect is already set to this area, so we don't need to set it ourselves
+      backgroundImage:draw( 0, 0 )
+    end
+  )
 end
 
 -- Now we'll call the function above to configure our game.
@@ -58,29 +63,29 @@ myGameSetUp()
 
 function playdate.update()
 
-    -- Poll the d-pad and move our player accordingly.
-    -- (There are multiple ways to read the d-pad; this is the simplest.)
-    -- Note that it is possible for more than one of these directions
-    -- to be pressed at once, if the user is pressing diagonally.
+  -- Poll the d-pad and move our player accordingly.
+  -- (There are multiple ways to read the d-pad; this is the simplest.)
+  -- Note that it is possible for more than one of these directions
+  -- to be pressed at once, if the user is pressing diagonally.
 
-    if playdate.buttonIsPressed( playdate.kButtonUp ) then
-        playerSprite:moveBy( 0, -2 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonRight ) then
-        playerSprite:moveBy( 2, 0 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonDown ) then
-        playerSprite:moveBy( 0, 2 )
-    end
-    if playdate.buttonIsPressed( playdate.kButtonLeft ) then
-        playerSprite:moveBy( -2, 0 )
-    end
+  if playdate.buttonIsPressed( playdate.kButtonUp ) then
+    playerSprite:moveBy( 0, -2 )
+  end
+  if playdate.buttonIsPressed( playdate.kButtonRight ) then
+    playerSprite:moveBy( 2, 0 )
+  end
+  if playdate.buttonIsPressed( playdate.kButtonDown ) then
+    playerSprite:moveBy( 0, 2 )
+  end
+  if playdate.buttonIsPressed( playdate.kButtonLeft ) then
+    playerSprite:moveBy( -2, 0 )
+  end
 
-    -- Call the functions below in playdate.update() to draw sprites and keep
-    -- timers updated. (We aren't using timers in this example, but in most
-    -- average-complexity games, you will.)
+  -- Call the functions below in playdate.update() to draw sprites and keep
+  -- timers updated. (We aren't using timers in this example, but in most
+  -- average-complexity games, you will.)
 
-    gfx.sprite.update()
-    -- playdate.timer.updateTimers()
+  gfx.sprite.update()
+  -- playdate.timer.updateTimers()
 
 end
