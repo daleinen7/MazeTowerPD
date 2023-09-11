18e2c358b0543f534874be7d272729eda500de4a
Doug Leinen <daleinen@gmail.com>
2023-09-10 21:51:36 -0500

    load old JS data for levels

--- Source/main.lua
+++ Source/main.lua
@@ -6,54 +6,77 @@ import "CoreLibs/math"
 import "CoreLibs/crank"
 import "CoreLibs/animation"
 
+local levels = require("levels")
+
 
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
-
+-- Set background to black
 gfx.setColor(gfx.kColorBlack)
 gfx.fillRect(0, 0, 400, 240)
 gfx.setBackgroundColor(gfx.kColorBlack)
 
-
+-- Import the tileset
 tilesheet = gfx.imagetable.new("Images/onebit")
-tiles = {
-	844,844,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-	0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-}
-
-tm = gfx.tilemap.new()
-tm:setImageTable(tilesheet)
-tm:setTiles(tiles, 4)
-tm:draw(0, 0)
 
 -- local player = Player(100, 60, gfx.imagetable.new("images/mazzy"))
 -- player:add()
 
-background = gfx.sprite.new(tm)
-background:setCenter(0, 0)
-background:add()
+currentLevel = 1
 
-function pd.update() 
-	
+-- Create an empty tilemap
+local tilemap = {}
+function buildLevel(level)
+  local tilesetKeyTranslation = {
+    walls = 844,
+    torches = 845,
+    ladders = 846,
+    holes = 847,
+    planks = 848,
+    coins = 849,
+    exit = 850
+  }
+
+  -- Create an empty tilemap
+  -- Initialize the tilemap with empty tiles
+  for i = 1, 225 do
+    tilemap[i] = 0
+  end
+
+  -- Iterate through each tile type needing added
+  for tile, positions in pairs(level) do
+    -- Iterate through each position needing added
+    for i = 1, #positions do
+      -- Add the tile to the tilemap
+
+      print("What is this note")
+      print(tile)
+      tilemap[positions[i]] = tile
+    end
+  end
+
+  tm = gfx.tilemap.new()
+  tm:setImageTable(tilesheet)
+  tm:setTiles(tilemap, 15)
+  tm:draw(0, 0)
+
+  background = gfx.sprite.new(tm)
+  background:setCenter(0, 0)
+  background:add()
+end
+
+-- buildLevel(levels[currentLevel])
+
+print(levels[1])
+
+function pd.update()
+
 	if pd.buttonJustPressed(pd.kButtonA) then
 		tm:setTileAtPosition(2, 2, 6)
 		tm:draw(0, 0)
 	end
-	
+
 	gfx.sprite.update()
 	pd.timer.updateTimers()
 end
